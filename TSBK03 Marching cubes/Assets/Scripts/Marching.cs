e1cf1ab6a0d73b8ffad2b68158968cf884feeb17
Jonas <jonas.langdahl97@gmail.com>
2020-11-05 10:45:35 +0100

    new marching cube script

--- TSBK03 Marching cubes/Assets/Scripts/Marching.cs
+++ TSBK03 Marching cubes/Assets/Scripts/Marching.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class Marching : MonoBehaviour
 {
 
-	MeshFilter meshFilter;
-
-	float terrainSurface = 0.5f;
-	int width = 32;
-	int height = 8;
-	float[,,] terrainMap;
-
 	List<Vector3> vertices = new List<Vector3>();
 	List<int> triangles = new List<int>();
 
@@ -25,78 +18,30 @@ public class Marching : MonoBehaviour
 
 	}
 
-	void CreateMeshData()
-	{
-
-		ClearMeshData();
-
-		// Loop through each "cube" in our terrain.
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				for (int z = 0; z < width; z++)
-				{
-
-					// Create an array of floats representing each corner of a cube and get the value from our terrainMap.
-					float[] cube = new float[8];
-					for (int i = 0; i < 8; i++)
-					{
-
-						Vector3Int corner = new Vector3Int(x, y, z) + CornerTable[i];
-						cube[i] = terrainMap[corner.x, corner.y, corner.z];
-
-					}
-
-					// Pass the value into our MarchCube function.
-					MarchCube(new Vector3(x, y, z), cube);
-
-				}
-			}
-		}
-
-		BuildMesh();
-
-	}
-
-	void PopulateTerrainMap()
-	{
-
-		// The data points for terrain are stored at the corners of our "cubes", so the terrainMap needs to be 1 larger
-		// than the width/height of our mesh.
-		for (int x = 0; x < width + 1; x++)
-		{
-			for (int z = 0; z < width + 1; z++)
-			{
-				for (int y = 0; y < height + 1; y++)
-				{
-
-					// Get a terrain height using regular old Perlin noise.
-					float thisHeight = (float)height * Mathf.PerlinNoise((float)x / 16f * 1.5f + 0.001f, (float)z / 16f * 1.5f + 0.001f);
-
-					float point = 0;
-					// We're only interested when point is within 0.5f of terrain surface. More than 0.5f less and it is just considered
-					// solid terrain, more than 0.5f above and it is just air. Within that range, however, we want the exact value.
-					if (y <= thisHeight - 0.5f)
-						point = 0f;
-					else if (y > thisHeight + 0.5f)
-						point = 1f;
-					else if (y > thisHeight)
-						point = (float)y - thisHeight;
-					else
-						point = thisHeight - (float)y;
-
-					// Set the value of this point in the terrainMap.
-					terrainMap[x, y, z] = point;
-
-				}
-			}
-		}
-	}
-
+	
+	// Performs Marching cubes on a single cube
+	// input: positon and the cubes 8 vertices
 	void MarchCube(Vector3 position, float[] cube)
 	{
+		int flagIndex, edgeIndex;
 
+		int flagIndex = 0;
+		// Find which corners/vertices are inside or outside the surface
+		for (v = 0; v < 8; v++)
+		{
+			if (cube[v] < 0)
+				// Shifts 1 to left by 'v' bits. v=3 -> 0000 1000 = 8
+				flagIndex |= 1 << v;
+		}
+
+		// If the configuration of this cube is 0 or 255 (completely inside the terrain or completely outside of it) we don't need to do anything.
+		if (flagIndex == 0 || flagIndex == 255)
+			return;
+
+		// Find which edges are intersected by the surface
+		edgeIndex = edgeTable[flagIndex]; // returns 12 bit number
+
+		/*
 		// Get the configuration index of this cube.
 		int configIndex = GetCubeConfiguration(cube);
 
@@ -132,6 +77,7 @@ public class Marching : MonoBehaviour
 
 			}
 		}
+		*/
 	}
 
 	int GetCubeConfiguration(float[] cube)
@@ -150,25 +96,6 @@ public class Marching : MonoBehaviour
 		}
 
 		return configurationIndex;
-
-	}
-
-	void ClearMeshData()
-	{
-
-		vertices.Clear();
-		triangles.Clear();
-
-	}
-
-	void BuildMesh()
-	{
-
-		Mesh mesh = new Mesh();
-		mesh.vertices = vertices.ToArray();
-		mesh.triangles = triangles.ToArray();
-		mesh.RecalculateNormals();
-		meshFilter.mesh = mesh;
 
 	}
 
@@ -201,6 +128,25 @@ public class Marching : MonoBehaviour
 		{ new Vector3(0.0f, 1.0f, 0.0f), new Vector3(0.0f, 1.0f, 1.0f) }
 
 	};
+
+	private int[] edgeTable = {
+		0x000, 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c, 0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
+		0x190, 0x099, 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c, 0x99c, 0x895, 0xb9f, 0xa96, 0xd9a, 0xc93, 0xf99, 0xe90,
+		0x230, 0x339, 0x033, 0x13a, 0x636, 0x73f, 0x435, 0x53c, 0xa3c, 0xb35, 0x83f, 0x936, 0xe3a, 0xf33, 0xc39, 0xd30,
+		0x3a0, 0x2a9, 0x1a3, 0x0aa, 0x7a6, 0x6af, 0x5a5, 0x4ac, 0xbac, 0xaa5, 0x9af, 0x8a6, 0xfaa, 0xea3, 0xda9, 0xca0,
+		0x460, 0x569, 0x663, 0x76a, 0x066, 0x16f, 0x265, 0x36c, 0xc6c, 0xd65, 0xe6f, 0xf66, 0x86a, 0x963, 0xa69, 0xb60,
+		0x5f0, 0x4f9, 0x7f3, 0x6fa, 0x1f6, 0x0ff, 0x3f5, 0x2fc, 0xdfc, 0xcf5, 0xfff, 0xef6, 0x9fa, 0x8f3, 0xbf9, 0xaf0,
+		0x650, 0x759, 0x453, 0x55a, 0x256, 0x35f, 0x055, 0x15c, 0xe5c, 0xf55, 0xc5f, 0xd56, 0xa5a, 0xb53, 0x859, 0x950,
+		0x7c0, 0x6c9, 0x5c3, 0x4ca, 0x3c6, 0x2cf, 0x1c5, 0x0cc, 0xfcc, 0xec5, 0xdcf, 0xcc6, 0xbca, 0xac3, 0x9c9, 0x8c0,
+		0x8c0, 0x9c9, 0xac3, 0xbca, 0xcc6, 0xdcf, 0xec5, 0xfcc, 0x0cc, 0x1c5, 0x2cf, 0x3c6, 0x4ca, 0x5c3, 0x6c9, 0x7c0,
+		0x950, 0x859, 0xb53, 0xa5a, 0xd56, 0xc5f, 0xf55, 0xe5c, 0x15c, 0x055, 0x35f, 0x256, 0x55a, 0x453, 0x759, 0x650,
+		0xaf0, 0xbf9, 0x8f3, 0x9fa, 0xef6, 0xfff, 0xcf5, 0xdfc, 0x2fc, 0x3f5, 0x0ff, 0x1f6, 0x6fa, 0x7f3, 0x4f9, 0x5f0,
+		0xb60, 0xa69, 0x963, 0x86a, 0xf66, 0xe6f, 0xd65, 0xc6c, 0x36c, 0x265, 0x16f, 0x066, 0x76a, 0x663, 0x569, 0x460,
+		0xca0, 0xda9, 0xea3, 0xfaa, 0x8a6, 0x9af, 0xaa5, 0xbac, 0x4ac, 0x5a5, 0x6af, 0x7a6, 0x0aa, 0x1a3, 0x2a9, 0x3a0,
+		0xd30, 0xc39, 0xf33, 0xe3a, 0x936, 0x83f, 0xb35, 0xa3c, 0x53c, 0x435, 0x73f, 0x636, 0x13a, 0x033, 0x339, 0x230,
+		0xe90, 0xf99, 0xc93, 0xd9a, 0xa96, 0xb9f, 0x895, 0x99c, 0x69c, 0x795, 0x49f, 0x596, 0x29a, 0x393, 0x099, 0x190,
+		0xf00, 0xe09, 0xd03, 0xc0a, 0xb06, 0xa0f, 0x905, 0x80c, 0x70c, 0x605, 0x50f, 0x406, 0x30a, 0x203, 0x109, 0x000
+};
 
 	private int[,] TriangleTable = new int[,] {
 
