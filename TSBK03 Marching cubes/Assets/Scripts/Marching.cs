dff824185bfc0f48817ac22407cdd912e756cfff
Jonas <jonas.langdahl97@gmail.com>
2020-11-12 10:35:39 +0100

    Working marching cubes
    One cube where the index can be changed.

--- TSBK03 Marching cubes/Assets/Scripts/Marching.cs
+++ TSBK03 Marching cubes/Assets/Scripts/Marching.cs
@@ -5,38 +5,70 @@ using UnityEngine;
 
 public class Marching : MonoBehaviour
 {
+	MeshFilter meshFilter;
+
 	List<Vector3> vertices = new List<Vector3>();
 	List<int> triangles = new List<int>();
-	public int _config = 1;
+	public int _config = -1;
 
-	private void Start()
-	{
+    private void Start()
+    {
 
-		meshFilter = GetComponent<MeshFilter>();
+		//meshFilter = GetComponent<MeshFilter>();
 		//terrainMap = new float[width + 1, height + 1, width + 1];
 		//PopulateTerrainMap();
 		//CreateMeshData();
+		meshFilter = GetComponent<MeshFilter>();
 
-		MarchCube(Vector3.zero, _config);
+		//float[] cube = { 0.0f, 0.5f, -1.0f, 0.2f, 0.5f, 0.1f, -0.3f, -0.9f };
+
+        //MarchCube(Vector3.zero, _config);
+		//BuildMesh();
+    }
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_config++;
+			ClearMesh();
+			MarchCube(Vector3.zero, _config);
+			BuildMesh();
+			Debug.Log("space pressed");
+		}
+	}
+
+	void BuildMesh()
+	{
+
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.RecalculateNormals();
+		meshFilter.mesh = mesh;
 
 	}
 
-	
+	void ClearMesh()
+    {
+		vertices.Clear();
+		triangles.Clear();
+    }
+
 	// Performs Marching cubes on a single cube
 	// input: positon and the cubes 8 vertices
-	void MarchCube(Vector3 position, int cubeIndex)//float[] cube)
+	void MarchCube(Vector3 position, int index)//float[] cube)
 	{
-		/*int cubeIndex, edgeIndex;
+		int cubeIndex = index;
 
-		int cubeIndex = 0;
 		// Find which corners/vertices are inside or outside the surface
-		for (v = 0; v < 8; v++)
-		{
-			if (cube[v] > 0)
-				// Shifts 1 to left by 'v' bits. v=3 -> 0000 1000 = 8
-				cubeIndex |= 1 << v;
-		}
-		*/
+		//for (int v = 0; v < 8; v++)
+		//{
+		//	if (cube[v] > 0)
+		//		// Shifts 1 to left by 'v' bits. v=3 -> 0000 1000 = 8
+		//		cubeIndex |= 1 << v;
+		//}
+		
 
 		// If the configuration of this cube is 0 or 255 (completely inside the terrain or completely outside of it) we don't need to do anything.
 		if (cubeIndex == 0 || cubeIndex == 255)
@@ -46,47 +78,34 @@ public class Marching : MonoBehaviour
 		//edgeIndex = edgeTable[flagIndex]; // returns 12 bit number 
 
 		// Find right set of triangles. Super advanced loop
-		for(int i = 0; TriangleTable[cubeIndex][i] != -1; i += 3)
+		for(int i = 0; i < 16; i += 3)
         {
-			Vector3 vert0a = EdgeTable[ TriangleTable[cubeIndex][i], 0];
-			Vector3 vert0b = EdgeTable[ TriangleTable[cubeIndex][i], 1];
+			if(TriangleTable[cubeIndex, i] == -1)
+            {
+				return;
+            }
 
-			Vector3 vert1a = EdgeTable[ TriangleTable[cubeIndex][i+1], 0];
-			Vector3 vert1b = EdgeTable[ TriangleTable[cubeIndex][i+1], 1];
+			Vector3 vert0a = position + EdgeTable[ TriangleTable[cubeIndex, i], 0];
+			Vector3 vert0b = position + EdgeTable[ TriangleTable[cubeIndex, i], 1];
 
-			Vector3 vert2a = EdgeTable[ TriangleTable[cubeIndex][i+2], 0];
-			Vector3 vert2b = EdgeTable[ TriangleTable[cubeIndex][i+2], 1];
+			Vector3 vert1a = position + EdgeTable[ TriangleTable[cubeIndex, i+1], 0];
+			Vector3 vert1b = position + EdgeTable[ TriangleTable[cubeIndex, i+1], 1];
+
+			Vector3 vert2a = position + EdgeTable[ TriangleTable[cubeIndex, i+2], 0];
+			Vector3 vert2b = position + EdgeTable[ TriangleTable[cubeIndex, i+2], 1];
 
 			// Create vertex in middle of each edge
-			Vector3 vert0 = (vert0a + vert0b) / 2;
-			Vector3 vert1 = (vert1a + vert1b) / 2;
-			Vector3 vert2 = (vert2a + vert2b) / 2;
+			Vector3 vert0 = (vert0a + vert0b) * 0.5f;
+			Vector3 vert1 = (vert1a + vert1b) * 0.5f;
+			Vector3 vert2 = (vert2a + vert2b) * 0.5f;
 
 			vertices.Add(vert0);
+			triangles.Add(vertices.Count - 1);
 			vertices.Add(vert1);
+			triangles.Add(vertices.Count - 1);
 			vertices.Add(vert2);
-
 			triangles.Add(vertices.Count - 1);
 		}
-	}
-
-	int GetCubeConfiguration(float[] cube)
-	{
-
-		// Starting with a configuration of zero, loop through each point in the cube and check if it is below the terrain surface.
-		int configurationIndex = 0;
-		for (int i = 0; i < 8; i++)
-		{
-
-			// If it is, use bit-magic to the set the corresponding bit to 1. So if only the 3rd point in the cube was below
-			// the surface, the bit would look like 00100000, which represents the integer value 32.
-			if (cube[i] > terrainSurface)
-				configurationIndex |= 1 << i;
-
-		}
-
-		return configurationIndex;
-
 	}
 
 	Vector3Int[] CornerTable = new Vector3Int[8] {
@@ -118,25 +137,6 @@ public class Marching : MonoBehaviour
 		{ new Vector3(0.0f, 1.0f, 0.0f), new Vector3(0.0f, 1.0f, 1.0f) }
 
 	};
-
-	private int[] edgeTable = {
-		0x000, 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c, 0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
-		0x190, 0x099, 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c, 0x99c, 0x895, 0xb9f, 0xa96, 0xd9a, 0xc93, 0xf99, 0xe90,
-		0x230, 0x339, 0x033, 0x13a, 0x636, 0x73f, 0x435, 0x53c, 0xa3c, 0xb35, 0x83f, 0x936, 0xe3a, 0xf33, 0xc39, 0xd30,
-		0x3a0, 0x2a9, 0x1a3, 0x0aa, 0x7a6, 0x6af, 0x5a5, 0x4ac, 0xbac, 0xaa5, 0x9af, 0x8a6, 0xfaa, 0xea3, 0xda9, 0xca0,
-		0x460, 0x569, 0x663, 0x76a, 0x066, 0x16f, 0x265, 0x36c, 0xc6c, 0xd65, 0xe6f, 0xf66, 0x86a, 0x963, 0xa69, 0xb60,
-		0x5f0, 0x4f9, 0x7f3, 0x6fa, 0x1f6, 0x0ff, 0x3f5, 0x2fc, 0xdfc, 0xcf5, 0xfff, 0xef6, 0x9fa, 0x8f3, 0xbf9, 0xaf0,
-		0x650, 0x759, 0x453, 0x55a, 0x256, 0x35f, 0x055, 0x15c, 0xe5c, 0xf55, 0xc5f, 0xd56, 0xa5a, 0xb53, 0x859, 0x950,
-		0x7c0, 0x6c9, 0x5c3, 0x4ca, 0x3c6, 0x2cf, 0x1c5, 0x0cc, 0xfcc, 0xec5, 0xdcf, 0xcc6, 0xbca, 0xac3, 0x9c9, 0x8c0,
-		0x8c0, 0x9c9, 0xac3, 0xbca, 0xcc6, 0xdcf, 0xec5, 0xfcc, 0x0cc, 0x1c5, 0x2cf, 0x3c6, 0x4ca, 0x5c3, 0x6c9, 0x7c0,
-		0x950, 0x859, 0xb53, 0xa5a, 0xd56, 0xc5f, 0xf55, 0xe5c, 0x15c, 0x055, 0x35f, 0x256, 0x55a, 0x453, 0x759, 0x650,
-		0xaf0, 0xbf9, 0x8f3, 0x9fa, 0xef6, 0xfff, 0xcf5, 0xdfc, 0x2fc, 0x3f5, 0x0ff, 0x1f6, 0x6fa, 0x7f3, 0x4f9, 0x5f0,
-		0xb60, 0xa69, 0x963, 0x86a, 0xf66, 0xe6f, 0xd65, 0xc6c, 0x36c, 0x265, 0x16f, 0x066, 0x76a, 0x663, 0x569, 0x460,
-		0xca0, 0xda9, 0xea3, 0xfaa, 0x8a6, 0x9af, 0xaa5, 0xbac, 0x4ac, 0x5a5, 0x6af, 0x7a6, 0x0aa, 0x1a3, 0x2a9, 0x3a0,
-		0xd30, 0xc39, 0xf33, 0xe3a, 0x936, 0x83f, 0xb35, 0xa3c, 0x53c, 0x435, 0x73f, 0x636, 0x13a, 0x033, 0x339, 0x230,
-		0xe90, 0xf99, 0xc93, 0xd9a, 0xa96, 0xb9f, 0x895, 0x99c, 0x69c, 0x795, 0x49f, 0x596, 0x29a, 0x393, 0x099, 0x190,
-		0xf00, 0xe09, 0xd03, 0xc0a, 0xb06, 0xa0f, 0x905, 0x80c, 0x70c, 0x605, 0x50f, 0x406, 0x30a, 0x203, 0x109, 0x000
-};
 
 	private int[,] TriangleTable = new int[,] {
 
